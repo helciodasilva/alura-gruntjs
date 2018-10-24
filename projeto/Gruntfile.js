1c7463ef00d8b1a72bac5c5ebdb1d0ffa8e0afde
Helcio da Silva <helciodasilva@gmail.com>
2018-10-23 23:39:27 -0300

    Aula 2

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -12,12 +12,25 @@ module.exports = function(grunt) {
               dist: {
                   src: ['dist']
               }
+         },
+
+         useminPrepare: {
+             html: 'dist/**/*.html'
+         },
+
+         usemin: {
+             html: 'dist/**/*.html'
          }
   });
 
-  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('default', ['dist', 'minifica']);
   grunt.registerTask('dist', ['clean', 'copy']);
-
+  grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-usemin');
+
 };
