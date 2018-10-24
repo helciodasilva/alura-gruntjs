3448a705a59564f18c7cbbfbd9d97a82006b4e0f
Helcio da Silva <helciodasilva@gmail.com>
2018-10-24 19:39:48 -0300

    Aula 4

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -29,18 +29,35 @@ module.exports = function(grunt) {
 				src: '**/*.{png,jpg,gif}',
 				dest: 'dist/img'
 			}
+		},
+
+         rev: {
+			options: {
+				enconding: 'utf8',
+				algorithm: 'md5',
+				length: 8
+			},
+
+			imagens: {
+				src: ['dist/img/**/*.{jpg,png,gif}']
+			},
+
+			minificados: {
+				src: ['dist/js/**/*.min.js', 'dist/css/**/*.min.css']
+			}
 		}
 
   });
 
   grunt.registerTask('default', ['dist', 'minifica']);
   grunt.registerTask('dist', ['clean', 'copy']);
-  grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'imagemin']);
+  grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'rev:imagens','rev:minificados', 'usemin', 'imagemin']);
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-usemin');
+  grunt.loadNpmTasks('grunt-rev');
 
 };
